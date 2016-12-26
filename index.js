c74083364effde662585b2a1712d18f6ae81673e
arahoomam <arahoomam@gmail.com>
2016-12-25 19:10:53 -0800

    added in http requests

--- index.js
+++ index.js
@@ -5,24 +5,45 @@ module.change_code = 1;
 'use strict';
 
 var alexa = require( 'alexa-app' );
+var http = require('http');
 var app = new alexa.app( 'cumtd' );
 
 //======================================================
 var req = require('request');
 
-var url = "https://developer.cumtd.com/api/v2.2/json/GetDeparturesByStop?stop_id=IT&key=bddb4d82cb704b708cdf32c72a58db04";
-var options = {
-    url: url
-};
+// var url = "https://developer.cumtd.com/api/v2.2/json/GetDeparturesByStop?stop_id=IT&key=bddb4d82cb704b708cdf32c72a58db04";
+// var options = {
+//     url: url
+// };
 
-var cb = function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body);
-        console.log(info);
-    }
-};
+// var cb = function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//         var info = JSON.parse(body);
+//         return info;
+//     }
+// };
 
-req(options, cb);
+// req(options, cb);
+
+
+function getJsonStops(stop_id, response, eventCallback) {
+    var url = "http://developer.cumtd.com/api/v2.2/json/GetDeparturesByStop?stop_id=IT&key=bddb4d82cb704b708cdf32c72a58db04";
+
+    http.get(url, function(res) {
+        var body = '';
+
+        res.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        res.on('end', function () {
+            var stringResult = JSON.parse(body);
+            eventCallback(response, stringResult);
+        });
+    }).on('error', function (e) {
+        console.log("Got error: ", e);
+    });
+}
 //======================================================
 
 app.launch( function( request, response ) {
@@ -52,5 +73,32 @@ app.intent('sayNumber',
         response.say("You asked for the number "+number);
     }
 );
+
+app.intent('getStop',
+    {
+        "slots":{"stop":"STOPS"}
+        ,"utterances":[
+        "Give me buses for stop {-|stop}"]
+    },
+    function(request,response) {
+        var stop = request.slot('stop');
+        if (stop === "illinois terminal") {
+            getJsonStops("IT", response, function(resp, https_out) {
+                if (https_out.departures.length === 0) {
+                    resp.say("No departures at this time")
+                } else {
+                    resp.say("ay lmao").send();
+                }
+                console.log(https_out);
+                resp.send();
+            });
+        } else {
+            response.say("You asked for the stop "+ stop);
+        }
+        return false;
+    }
+);
+
+
 
 module.exports = app;
